7ca23ad515233aee5511d970863d95ae9cad2344
DavidOrtego <davidortego2016@gmail.com>
2026-02-26 16:19:56 +0100

    feat: enhance integration tests for companies API with additional checks and data validation

--- src/test/integration/companies.test.js
+++ src/test/integration/companies.test.js
@@ -4,7 +4,15 @@ const { db } = require('../../configuration/database');
 
 describe('Integration tests for companies API', () => {
   beforeAll(async () => {
+    await db.raw('SET FOREIGN_KEY_CHECKS = 0');
+
+    await db('videogame_console').truncate();
+    await db('videogames').truncate();
     await db('companies').truncate();
+    await db('consoles').truncate();
+
+    await db.raw('SET FOREIGN_KEY_CHECKS = 1');
+
     await db('companies').insert({
       id: 1,
       name: 'Nintendo',
@@ -18,5 +26,20 @@ describe('Integration tests for companies API', () => {
 
   afterAll(async () => {
     await db.destroy();
+  });
+
+  describe('GET /companies', () => {
+    test('should return a list of companies', async () => {
+      const response = await request(app).get('/companies');
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.title).toBe('success');
+      expect(response.body.message).toBe('Companies retrieved successfully');
+      expect(response.body).toHaveProperty('data');
+      expect(Array.isArray(response.body.data)).toBe(true);
+
+      expect(response.body.data[0].name).toBe('Nintendo');
+      expect(response.body.data[0]).toHaveProperty('yearsSinceFounded');
+    });
   });
 });
